bdbdb44cb3ad64f5871c214cbfbcd2afec522896
alexander mehertab <118993330+alex0286@users.noreply.github.com>
2022-12-07 10:27:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    variable_alex += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(variable_alex)
+})
+input.onButtonPressed(Button.B, function () {
+    variable_alex += -1
+})
+let variable_alex = 0
+variable_alex = 0
